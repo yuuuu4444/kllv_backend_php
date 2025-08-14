f5e527dc2544c470b4b5d14899526ad8efddfb2b
yuuuu4444 <yuuuu850430@gmail.com>
2025-08-14 16:14:19 +0800

    feat(server/local):新增server/local兩類資料夾，並將conn、cors分成本地端與伺服器端兩版，分別放入資料夾，以便env_init.php判斷

---
+++ common/local/conn.php
@@ -0,0 +1,25 @@
+<?php
+// 錯誤回報設定
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// 引入建立的設定檔
+require_once  __DIR__ . '/config.php';
+
+try {
+    // 使用 config.php 中定義的常數來連線
+    $mysqli = new mysqli(
+        DB_HOST, 
+        DB_USER, 
+        DB_PASS, 
+        DB_NAME,
+        DB_PORT
+    );
+    // echo "資料庫連線成功";
+} catch (mysqli_sql_exception $e) {
+    echo '資料庫連線錯誤：' . $e->getMessage();
+    exit();
+}
+?>
+
+
---
+++ common/local/cors.php
@@ -0,0 +1,27 @@
+<?php 
+// 引入建立的設定檔
+require_once __DIR__ . '/config.php';
+
+// 從設定檔讀取允許的網域列表
+$allowed_origins = ALLOWED_ORIGINS;
+
+// 抓到請求的來源網域
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+// 檢查來源是否存在於允許列表中
+// if (in_array($origin, $allowed_origins)) {
+//     header("Access-Control-Allow-Origin: " . $origin);
+// }
+
+// 允許所有網域連線
+header("Access-Control-Allow-Origin: " . "*");
+
+// 設定其他 CORS 標頭
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // 建議加上 Authorization，未來做登入驗證會用到
+
+// 如果是預檢請求，直接結束
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit();
+}
+?>
