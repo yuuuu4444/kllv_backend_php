2f07ed514a4ac28faf36f1d9b7da938b53a5afad
goobo251111111 <goobo251111111@gmail.com>
2025-08-23 20:45:32 +0800

    refactor(check_session): 修改 API

--- api/login/check_session.php
+++ api/login/check_session.php
@@ -1,14 +1,19 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+
+require_once __DIR__ . '/../../common/env_init.php';
+
 // Session 安全性設定
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.cookie_secure', 1);
+
 session_set_cookie_params([
     'samesite' => 'Strict'
 ]);
 
 session_start();
-header('Content-Type: application/json; charset=utf-8');
 
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
     echo json_encode([
