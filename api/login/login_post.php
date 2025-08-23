83453b5db1907da7c1e24bc59661ffbfbc6a4d3e
goobo251111111 <goobo251111111@gmail.com>
2025-08-23 17:30:58 +0800

    refactor(login): 修改登入 API

--- api/login/login_post.php
+++ api/login/login_post.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../../common/env_init.php';
 // Session 安全性設定
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_only_cookies', 1);
-ini_set('session.cookie_secure', 0); // 本地測試改為 0
+ini_set('session.cookie_secure', 1);
 
 session_set_cookie_params([
     'samesite' => 'Strict'
@@ -33,14 +33,43 @@ if (!$user_id || !$password) {
     exit;
 }
 
-$sql = "SELECT password, is_active FROM users WHERE user_id = ?";
+$sql = "SELECT
+        user_id,
+        fullname,
+        nickname,
+        password,
+        profile_image,
+        phone_number,
+        email,
+        id_number,
+        birth_date,
+        gender,
+        household_no,
+        role_type,
+        is_active
+        FROM users
+        WHERE user_id = ?";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param('s', $user_id);
 $stmt->execute();
-$stmt->bind_result($db_password, $is_active);
+$stmt->bind_result(
+    $db_user_id,
+    $db_fullname,
+    $db_nickname,
+    $db_password,
+    $db_profile_image,
+    $db_phone_number,
+    $db_email,
+    $db_id_number,
+    $db_birth_date,
+    $db_gender,
+    $db_household_no,
+    $db_role_type,
+    $db_is_active
+);
 
 if ($stmt->fetch()) {
-    if (!$is_active) {
+    if (!$db_is_active) {
         http_response_code(403);
         echo json_encode(['status'=>'error','message'=>'帳號尚未啟用'], JSON_UNESCAPED_UNICODE);
         exit;
@@ -60,7 +89,17 @@ if ($stmt->fetch()) {
         'message' => '登入成功',
         'data' => [
             'user' => [
-                'user_id' => $user_id
+								'user_id'       => $db_user_id,
+                'fullname'      => $db_fullname,
+                'nickname'      => $db_nickname,
+                'profile_image' => $db_profile_image,
+                'phone_number'  => $db_phone_number,
+                'email'         => $db_email,
+                'id_number'     => $db_id_number,
+                'birth_date'    => $db_birth_date,
+                'gender'        => $db_gender,
+                'household_no'  => $db_household_no,
+                'role_type'     => $db_role_type
             ]
         ]
     ], JSON_UNESCAPED_UNICODE);
