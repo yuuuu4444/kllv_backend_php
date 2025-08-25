8b9c937e5675576eb852de0b29350c02377e499c
yuuuu4444 <yuuuu850430@gmail.com>
2025-08-25 10:04:45 +0800

    refactor(events_regs_get.php): 修改-取得活動報名api-判斷登入 refactor(cancel_reasons_get.php): 修改-取得取消原因api refactor(event_cancel_post.php): 修改-取消活動報名api-判斷登入

--- api/member/cancel_reasons_get.php
+++ api/member/cancel_reasons_get.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once __DIR__ . '/../../common/env_init.php';
 
+
+// 設定 HTTP Header
 header('Content-Type: application/json; charset=utf-8');
 if ($_SERVER["REQUEST_METHOD"] !== "GET") {
     throw new Exception("僅支援 GET 方法", 405);
@@ -14,8 +16,8 @@ $stmt = null;
 
 try {
     $sql = "SELECT reason_no, reason_name 
-              FROM events_regs_cancel_reasons 
-              ORDER BY reason_no ASC";
+            FROM events_regs_cancel_reasons 
+            ORDER BY reason_no ASC";
     
     $stmt = $mysqli->prepare($sql);
     if ($stmt === false) {
