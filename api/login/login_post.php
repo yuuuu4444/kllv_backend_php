e4ecdb84de1313d5d3fced16c238b23e2aa5346e
goobo251111111 <goobo251111111@gmail.com>
2025-08-25 17:25:12 +0800

    test(login): 測試

--- api/login/login_post.php
+++ api/login/login_post.php
@@ -4,14 +4,14 @@
 
 	require_once __DIR__ . '/../../common/env_init.php';
 
-	Session 安全性設定
-	ini_set('session.cookie_httponly', 1);
-	ini_set('session.use_only_cookies', 1);
-	ini_set('session.cookie_secure', 1);
+	// Session 安全性設定
+	// ini_set('session.cookie_httponly', 1);
+	// ini_set('session.use_only_cookies', 1);
+	// ini_set('session.cookie_secure', 1);
 
-	session_set_cookie_params([
-			'samesite' => 'Strict'
-	]);
+	// session_set_cookie_params([
+	// 		'samesite' => 'Strict'
+	// ]);
 
 	if (session_status() === PHP_SESSION_NONE) {
 			session_start();
